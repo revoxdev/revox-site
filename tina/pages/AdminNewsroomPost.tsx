5e12cf020c5c3a260bb9499820b22ff906f2991e
revox-dev <dev@revox.io>
2025-09-26 13:57:57 +0530

    feat: Done web and mobile responsive blog

--- tina/pages/AdminNewsroomPost.tsx
+++ tina/pages/AdminNewsroomPost.tsx
@@ -1,59 +1,118 @@
-import React from 'react';
+import React from "react";
 import { tinaField, useTina } from "tinacms/dist/react";
-import type { NewsroomQuery, NewsroomQueryVariables } from '../__generated__/types';
-import { TinaMarkdown } from 'tinacms/dist/rich-text'
-import FormattedDate from '../../src/components/react/FormattedDate.tsx';
+import type {
+  NewsroomQuery,
+  NewsroomQueryVariables,
+} from "../__generated__/types";
+import { TinaMarkdown } from "tinacms/dist/rich-text";
+import FormattedDate from "../../src/components/react/FormattedDate.tsx";
+import "../../src/styles/blog.detail.css";
 
 type Props = {
-	variables: NewsroomQueryVariables;
-	data: NewsroomQuery;
-	query: string;
-}
+  variables: NewsroomQueryVariables;
+  data: NewsroomQuery;
+  query: string;
+};
 
 export default function AdminNewsroomPost(props: Props) {
+  const { data } = useTina({
+    query: props.query,
+    variables: props.variables,
+    data: props.data,
+  });
 
-	const { data } = useTina({
-		query: props.query,
-		variables: props.variables,
-		data: props.data,
-	})
+  const newsroom = data.newsroom;
 
-	const newsroom = data.newsroom;
-
-	return (
-		<article>
-			<div data-tina-field={tinaField(newsroom, "heroImage")} className="hero-image">
-				{newsroom.heroImage && <img width={1020} height={510} src={newsroom.heroImage} alt="" />}
-			</div>
-			<div className="prose-wrapper">
-				<div className="prose">
-					<div className="title">
-						<div className="date-text" data-tina-field={tinaField(newsroom, "pubDate")} >
-							<FormattedDate date={newsroom.pubDate} />
-							{
-								newsroom.updatedDate && (
-									<div className="last-updated-on" data-tina-field={tinaField(newsroom, "updatedDate")} >
-										Last updated on <FormattedDate date={newsroom.updatedDate} />
-									</div>
-								)
-							}
-						</div>
-						<h1 data-tina-field={tinaField(newsroom, "title")} >{newsroom.title}</h1>
-						<ul className="tag-list-class">
-						{newsroom.tags && Array.isArray(newsroom.tags) ? (
-							newsroom.tags.map((tag) => (
-								<li className="tag">
-									#{tag}
-								</li>
-							))
-						):''}
-						</ul>
-					</div>
-					<div data-tina-field={tinaField(newsroom, "body")}>
-						<TinaMarkdown content={newsroom.body} />
-					</div>
-				</div>
-			</div>
-		</article>
-	);
+  return (
+    <article className="blog-detail-page">
+      <div
+        data-tina-field={tinaField(newsroom, "heroImage")}
+        className="hero-image"
+      >
+        {newsroom.heroImage && (
+          <img width={1020} height={510} src={newsroom.heroImage} alt="" />
+        )}
+      </div>
+      <div className="prose-wrapper-with-sidebar">
+        <div className="prose">
+          <div className="title">
+            <span data-tina-field={tinaField(newsroom, "title")}>
+              {newsroom.title}
+            </span>
+          </div>
+          <div className="author-info-view">
+            {newsroom.authors &&
+              newsroom.authors.length > 0 &&
+              newsroom.authors[0] && (
+                <>
+                  <div className="author-avatar">
+                    {newsroom.authors[0].avatar && (
+                      <img
+                        src={newsroom.authors[0].avatar}
+                        alt={newsroom.authors[0].name || "Author"}
+                      />
+                    )}
+                  </div>
+                  <div className="author-details">
+                    <span
+                      className="author-name"
+                      data-tina-field={tinaField(newsroom.authors[0], "name")}
+                    >
+                      {newsroom.authors[0].name}
+                    </span>
+                    <span
+                      className="author-designation"
+                      data-tina-field={tinaField(
+                        newsroom.authors[0],
+                        "designation"
+                      )}
+                    >
+                      {newsroom.authors[0].designation}
+                    </span>
+                  </div>
+                </>
+              )}
+          </div>
+          <div className="blog-content" data-tina-field={tinaField(newsroom, "body")}>
+            <TinaMarkdown content={newsroom.body} />
+          </div>
+        </div>
+        <div className="author-sidebar">
+          <div className="author-info-view">
+            {newsroom.authors &&
+              newsroom.authors.length > 0 &&
+              newsroom.authors[0] && (
+                <>
+                  <div className="author-avatar">
+                    {newsroom.authors[0].avatar && (
+                      <img
+                        src={newsroom.authors[0].avatar}
+                        alt={newsroom.authors[0].name || "Author"}
+                      />
+                    )}
+                  </div>
+                  <div className="author-details">
+                    <span
+                      className="author-name"
+                      data-tina-field={tinaField(newsroom.authors[0], "name")}
+                    >
+                      {newsroom.authors[0].name}
+                    </span>
+                    <span
+                      className="author-designation"
+                      data-tina-field={tinaField(
+                        newsroom.authors[0],
+                        "designation"
+                      )}
+                    >
+                      {newsroom.authors[0].designation}
+                    </span>
+                  </div>
+                </>
+              )}
+          </div>
+        </div>
+      </div>
+    </article>
+  );
 }
